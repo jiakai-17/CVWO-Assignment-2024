2198975c30665b89d9c6c7c539719271341c3120
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2023-12-13 13:14:02 +0800

    feat: Add login and signup buttons to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,9 +11,13 @@ import Link from "next/link";
 
 export default function Navbar() {
 
-  const [isLogin, setIsLogin] = useState(false);
+  const currentLoginState = localStorage.getItem("isLogin") ?? "false";
+
+  const [isLogin, setIsLogin] = useState(JSON.parse(currentLoginState));
+  localStorage.setItem("isLogin", currentLoginState);
 
   function toggleLogin() {
+    localStorage.setItem("isLogin", JSON.stringify(!isLogin));
     setIsLogin(!isLogin);
   }
 
@@ -23,17 +27,21 @@ export default function Navbar() {
   }
 
   return (
-    <Box sx={{ flexGrow: 1, mb: 8 }}>
+    <Box sx={{ mb: 8 }}>
       <AppBar position="static">
         <Toolbar>
-          <Box sx={{ flexGrow: 1, px: 2 }}>
+          <Box sx={{ px: 2 }}>
             <Link href={"/"} style={{ textDecoration: "none", color: "#000" }}>
               <Typography variant="h6" sx={{ color: "white" }}>
                 FORUM
               </Typography>
             </Link>
           </Box>
-          <Button color="inherit" onClick={handleLoginToggle}>
+          <div style={{ flexGrow: 1 }} />
+          {!isLogin && <Button color="inherit">
+            Signup
+          </Button>}
+          <Button color="inherit" onClick={handleLoginToggle} sx={{ ml: 2 }}>
             {isLogin ? "Logout" : "Login"}
           </Button>
         </Toolbar>
